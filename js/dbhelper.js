f4efb78dedb71e2759e6e6c55317e139d0dabd06
Sean Husmann <shindoh@live.de>
2018-04-06 09:55:33 +0200

    feat: Change URL for pulling restaurants to point to new server

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `/data/restaurants.json`;
+    return `http://localhost:1337/restaurants`;
   }
 
   /**
@@ -20,8 +20,7 @@ class DBHelper {
     xhr.open('GET', DBHelper.DATABASE_URL);
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+        const restaurants = JSON.parse(xhr.responseText);
         callback(null, restaurants);
       } else { // Oops!. Got an error from server.
         const error = (`Request failed. Returned status of ${xhr.status}`);
